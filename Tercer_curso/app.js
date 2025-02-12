350876277b91ef6a73f28e176d78e656091c38e3
Axodaraph <darielnotion@gmail.com>
2025-02-11 21:02:12 -0500

    api REST

---
+++ Tercer_curso/app.js
@@ -0,0 +1,106 @@
+const express = require('express') // require -> commonJS
+const movies = require('./movie.json')
+const crypto = require('node:crypto')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+
+const app = express()
+app.use(express.json())
+app.disable('x-powered-by')
+
+// metodos normales: GET/HEAD/POST
+// metodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+// todos los recursos que sean movies
+app.get('/movies', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+// recuperar una pelicula por su id
+app.get('/movies/:id', (req, res) => {
+  // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+// crear una pelicula
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // en base de datos
+
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+  }
+  // Esto no seria REST, porque estamos guardando el estado de la aplicacion en memoria
+  movies.push(newMovie)
+  res.status(201).json(newMovie) // actualizar la cache del cliente
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+app.options('/movies/:id', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+  res.send(200)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
